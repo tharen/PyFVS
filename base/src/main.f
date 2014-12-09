bc61ee4cd87adc06a23ca4f85fd374211f188559
tod haren <tod.haren@gmail.com>
2014-12-09 07:27:50 -0800

    SVN r1381

--- base/src/main.f
+++ base/src/main.f
@@ -1,9 +1,30 @@
       PROGRAM MAIN
       IMPLICIT NONE
 C----------
-C  $Id: main.f 829 2013-05-08 20:55:27Z rhavis@msn.com $
+C  $Id: main.f 1381 2014-12-09 15:26:03Z tod.haren@gmail.com $
 C----------
       INTEGER rtnCode,lenCL,i
+
+#ifdef PROFILING
+      ! In profiling mode multiple iterations of the same keyword
+      ! file can be run to increase the sample size for gprof
+      integer :: iters,j
+      character(len=32) :: arg
+
+      iters = 1
+      do j=0, command_argument_count()
+        call get_command_argument(j,arg)
+        if (len_trim(arg)==0) exit
+        if ((arg=='--iters') .or. (arg=='-i')) then
+            call get_command_argument(j+1,arg)
+            read(arg, '(i10)'), iters
+            print '(a i4)', 'Number of iterations:',iters
+        endif
+      enddo
+
+      do j=1, iters
+#endif
+
 C
 C     PROCSS THE COMMAND LINE. Passing an empty string signals that the 
 C     real command line arguments will be fetched.
@@ -23,7 +44,13 @@ C     RUN ALL THE CYCLES and STANDS--unless there is a stop point!
    
       call fvsGetICCode(i)
 
-      IF (i .EQ. 0) STOP	
+#ifdef PROFILING
+      print '(a i4)',"End of run: ",j
+      if (i > 0) exit
+      enddo
+#endif
+
+      IF (i .EQ. 0) STOP
 
       GO TO (11,12,13,14,15), i 
    11 CONTINUE
