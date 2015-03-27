9ec4d1f2cad3c2b03762960cba90c6fcd11abafb
Tod Haren <tod.haren@gmail.com>
2015-03-27 10:42:46 -0700

    Begin adding Doxygen documentation.

--- pn/src/htcalc.f
+++ pn/src/htcalc.f
@@ -1,3 +1,14 @@
+!> Calculate site height.
+!> Calculate potential height given a species site and age.
+!! It is used to calculate potential height and site.
+!! White oak is not base directly on SI and Age
+!! @param sindx Site Index
+!! @param ispc FVS Species index
+!! @param ag Tree age
+!! @param hguess Estimated potential height
+!! @param jostnd File unit number for debug messages
+!! @param debug Debgging flag
+
       SUBROUTINE HTCALC(SINDX,ISPC,AG,HGUESS,JOSTND,DEBUG)
       IMPLICIT NONE
 
