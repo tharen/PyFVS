46375a68160b3fd55a1c8c558ac779630eabec4a
Tod Haren <tod.haren@gmail.com>
2015-12-23 15:08:43 -0800

    Debuging Travis-CI

--- build_pyfvs.sh
+++ build_pyfvs.sh
@@ -5,7 +5,7 @@ mkdir travis-ci
 cd travis-ci
 
 cmake -G"MSYS Makefiles" .. -DFVS_VARIANTS="pnc;wcc" -DWITH_PYMOD=Yes \
-    -DPYTHON_EXECUTABLE=python35 \
+    -DPYTHON_EXECUTABLE=python27 \
     -D32BIT_TARGET=No -DNATIVE_ARCH=Yes -DSTATIC_LINK=ON \
     -DENABLE_LTO=OFF -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_PREFIX=(pwd)/Open-FVS
