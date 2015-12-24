56d5e7b621301e701552d8dfdc6d478135cba399
Tod Haren <tod.haren@gmail.com>
2015-12-23 16:23:41 -0800

    Update for virtualenv.

--- build_pyfvs.sh
+++ build_pyfvs.sh
@@ -5,7 +5,6 @@ mkdir -p travis-ci
 cd travis-ci
 
 cmake -G"Unix Makefiles" .. -DFVS_VARIANTS="pnc;wcc" -DWITH_PYMOD=Yes \
-    -DPYTHON_EXECUTABLE=python3.5 \
     -D32BIT_TARGET=No -DNATIVE_ARCH=Yes -DSTATIC_LINK=ON \
     -DENABLE_LTO=OFF -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_PREFIX=$(pwd)/Open-FVS
