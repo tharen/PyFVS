827e417da02ddcb2b5f62397d0d069e4db345c84
Tod Haren <tod.haren@gmail.com>
2015-12-28 14:19:42 -0800

    Add alternate F2PY arguments for building in MSYS2 on Windows.

--- bin/build_ext_py3.sh
+++ bin/build_ext_py3.sh
@@ -1,2 +1,11 @@
-f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $PROJ_ROOT/api/fvs_step.f90 
-f2py -c ./pyfvs$1.pyf -L. -lFVS$1_static -lodbc
+if [ -n ${MSYSTEM+x} ]; then
+  # Assume this is running on Windows under MSYS2/bash
+  f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $PROJ_ROOT/api/fvs_step.f90 
+  f2py -c ./pyfvs$1.pyf --compiler=mingw32 --fcompiler=gnu95 -L. -lFVS$1_static -lodbc32
+
+else
+  f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $PROJ_ROOT/api/fvs_step.f90 
+  f2py -c ./pyfvs$1.pyf -L. -lFVS$1_static -lodbc
+
+fi
+
