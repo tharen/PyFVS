434ff37adbbb7962f602b12cbff8a10ae32f51a9
Tod Haren <tod.haren@gmail.com>
2015-12-28 12:20:02 -0800

    Whitespace, EOL.

--- config_build.sh
+++ config_build.sh
@@ -4,7 +4,7 @@ cd bin
 mkdir -p travis-ci
 cd travis-ci
 
-cmake -G"Unix Makefiles" .. -DFVS_VARIANTS="pnc;wcc" -DWITH_PYMOD=Yes \
+cmake -G"Unix Makefiles" .. -DFVS_VARIANTS="pnc;wcc" -DWITH_PYMOD=OFF \
     -D32BIT_TARGET=No -DNATIVE_ARCH=Yes -DSTATIC_LINK=ON \
     -DENABLE_LTO=OFF -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_PREFIX=$(pwd)/Open-FVS
