7a8dab7039d233a7f8e9467f4aaf45bc6149a0c3
Tod Haren <tod.haren@gmail.com>
2015-12-28 05:07:32 +0000

    Debug F2PY build.

--- bin/build_ext_py3.sh
+++ bin/build_ext_py3.sh
@@ -1,2 +1,2 @@
-f2py3 -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature ../../../api/fvs_step.f90 
-f2py3 -c ./pyfvs$1.pyf -L. -lFVS$1_static -lodbc
+f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature ../../../api/fvs_step.f90 
+f2py -c ./pyfvs$1.pyf -L. -lFVS$1_static -lodbc
