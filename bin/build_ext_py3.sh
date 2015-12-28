409c6ebc1d42ef4cd35e25f8e09bd57d08a53ca0
Tod Haren <tod.haren@gmail.com>
2015-12-28 06:03:53 +0000

    Edit CI build scripts.

--- bin/build_ext_py3.sh
+++ bin/build_ext_py3.sh
@@ -1,2 +1,2 @@
-f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature ../../../api/fvs_step.f90 
+f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature /home/travis/build/tharen/PyFVS/api/fvs_step.f90 
 f2py -c ./pyfvs$1.pyf -L. -lFVS$1_static -lodbc
