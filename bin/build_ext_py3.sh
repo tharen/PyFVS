d32860db22e830074ec7b431c32edcfc270151a5
Tod Haren <tod.haren@gmail.com>
2015-12-28 15:15:34 -0800

    Edit build scripts.

--- bin/build_ext_py3.sh
+++ bin/build_ext_py3.sh
@@ -1,4 +1,5 @@
-if [ -n ${MSYSTEM+x} ]; then
+if [ -v MSYSTEM ]
+then
   # Assume this is running on Windows under MSYS2/bash
   f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $PROJ_ROOT/api/fvs_step.f90 
   f2py -c ./pyfvs$1.pyf --compiler=mingw32 --fcompiler=gnu95 -L. -lFVS$1_static -lodbc32
