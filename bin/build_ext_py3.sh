89092989c0645f15594e15b2a112b4b788cdba1f
Tod Haren <tod.haren@gmail.com>
2016-01-08 14:50:32 -0800

    Adds CI configuration and scripts from testing.

--- bin/build_ext_py3.sh
+++ bin/build_ext_py3.sh
@@ -1,14 +1,29 @@
 sources=$(cat pymod_source.txt)
+logfile=../f2py_$1.log
 
 if [ -v MSYSTEM ]
 then
   # Assume this is running on Windows under MSYS2/bash
-  $F2PY -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $sources
-  $F2PY -c --compiler=mingw32 --fcompiler=gnu95 -lodbc32 ./pyfvs$1.pyf libFVS$1_static.a
+  f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $sources > $logfile 2>&1
+  f2py -c --compiler=mingw32 --fcompiler=gnu95 -lodbc32 ./pyfvs$1.pyf libFVS$1_static.a >> $logfile 2>&1
 
 else
-  $F2PY -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $sources
-  $F2PY -c -lodbc ./pyfvs$1.pyf libFVS$1_static.a
+  
+  # Prefer the Python 3 version of F2PY  
+  py_ver=$(python -c "import sys;print(sys.version_info[0])")
+  if [ $py_ver = 3 ] || [ -e f2py3 ]; then
+    f2py=f2py3
+  else
+    f2py=f2py
+  fi
+  
+  i=$(which python)
+  echo Python executable: $i > $logfile
+  i=$(which $f2py)
+  echo F2PY executable: $i >> $logfile
+  
+  $f2py -h pyfvs$1.pyf -m pyfvs$1 --overwrite-signature $sources >> $logfile 2>&1
+  $f2py -c -lodbc ./pyfvs$1.pyf libFVS$1_static.a >> $logfile 2>&1
 
 fi
 
--- bin/setup.py
+++ bin/setup.py
@@ -1,5 +1,6 @@
 from __future__ import division, absolute_import, print_function
 
+import os
 from numpy.distutils.core import Extension
 
 fvs_variant = 'pnc'
@@ -9,10 +10,15 @@ ext_pyf = './pyfvspnc.pyf'
 fvs_lib = 'FVSpnc_static'
 lib_dir = '.'
 
+if os.name=='nt':
+    odbc = 'odbc32'
+else:
+    odbc = 'odbc'
+    
 ext1 = Extension(
         name = ext_name,
         sources = [ext_pyf,],
-        libraries = [fvs_lib,'odbc'],
+        libraries = [fvs_lib,odbc],
         library_dirs = [lib_dir,],
         #extra_compile_args = ['-J={}'.format(mod_path),]
 )
@@ -23,5 +29,6 @@ if __name__ == "__main__":
           description       = "F2PY wrappers for the Open-FVS library ({})".format(fvs_variant.upper()),
           author            = "Tod Haren",
           author_email      = "tod.haren<at>gm....com",
-          ext_modules = [ext1,]
+          ext_modules = [ext1,],
+          build_ext = ['--compiler=mingw32','--fcompiler=gnu',]
           )
