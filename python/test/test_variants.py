aedf66fe43140b18bed390ada468c07e538b6eae
Tod Haren <tod.haren@gmail.com>
2016-01-14 02:25:12 -0800

    Add parameterized tests to run each supported variant.
        Add test to verify growth routines run and return without runtime errors.
    
        closes #2

--- python/test/test_variants.py
+++ python/test/test_variants.py
@@ -14,6 +14,30 @@ variants = [
         ('pnc_basic', 'pnc'),
         ('wcc_basic', 'wcc')]
 
+def nw_bg_test_kwds():
+    kwds = """STDIDENT
+TEST_BG   Testing a simple bare ground run
+STDINFO          712    CHS131         0       200        35         6
+MANAGED            0         1
+
+INVYEAR         2010
+DESIGN             0         1       999         1         0         1         1
+
+NUMCYCLE          10
+
+NOTREES
+ESTAB           2010
+PLANT           2011        DF       450        95         2         1         0
+END
+
+ECHOSUM
+
+PROCESS
+STOP
+
+"""
+    return kwds
+
 class VariantTests(unittest.TestCase):
     @parameterized.expand(variants)
     def test_load_variant(self, name, variant):
@@ -23,3 +47,19 @@ class VariantTests(unittest.TestCase):
     def test_variant_name(self, name, variant):
         f = fvs.FVS(variant)
         self.assertEqual(f.variant, variant, 'Variant name failed')
+
+    @parameterized.expand(variants)
+    def test_fsv_step_fvs_grow(self, name, variant):
+        f = fvs.FVS(variant)
+        p = './bg_test.key'
+        with open(p, 'w') as foo:
+            foo.write(nw_bg_test_kwds())
+        f.fvs_step.fvs_init(p)
+
+        for c in range(f.contrl_mod.ncyc):
+            r = f.fvs_step.fvs_grow()
+
+        r = f.fvs_step.fvs_end()
+        print('FVS Return Code: %s' % r)
+        self.assertEqual(r, 0, 'FVS Return Code: %s' % r)
+
