a89124bd2f146098c57a1cb3e7e9bb06ca999c56
Tod Haren <tod.haren@gmail.com>
2016-01-14 12:45:22 -0800

    Install nose-parameterized in test scripts with pip.

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -1,3 +1,5 @@
+pip install --upgrade nose-parameterized
+
 set PYTHONPATH=%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python;%PYTHONPATH%
 
 pushd %APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python\test
--- .travis-ci/run_tests.sh
+++ .travis-ci/run_tests.sh
@@ -1,3 +1,5 @@
+pip install --upgrade nose-parameterized
+
 export PYTHONPATH=${BUILD_ROOT}/bin/build/Open-FVS/python:$PYTHONPATH
 
 pushd ${BUILD_ROOT}/bin/build/Open-FVS/test
