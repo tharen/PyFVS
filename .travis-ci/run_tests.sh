035117f7b84a5af4f47777130b66d245974e315c
Tod Haren <tod.haren@gmail.com>
2016-01-14 13:02:27 -0800

    Fix path error in call to test scripts.

--- .travis-ci/run_tests.sh
+++ .travis-ci/run_tests.sh
@@ -2,6 +2,6 @@ pip install --upgrade nose-parameterized
 
 export PYTHONPATH=${TRAVIS_BUILD_DIR}/bin/build/Open-FVS/python:$PYTHONPATH
 
-pushd ${TRAVIS_BUILD_DIR}/bin/build/Open-FVS/test
+pushd ${TRAVIS_BUILD_DIR}/bin/build/Open-FVS/python/test
 python -m unittest test_variants
 popd
