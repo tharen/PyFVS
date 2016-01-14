b634565c58c3c8d54c6761ccedcfd11d840b8c60
Tod Haren <tod.haren@gmail.com>
2016-01-14 13:26:02 -0800

    Fix call to python in appveyor test script.

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -3,6 +3,6 @@ pip install --upgrade nose-parameterized
 set PYTHONPATH="%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python;%PYTHONPATH%"
 
 pushd "%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python\test"
-call %PYTHON% -m unittest test_variants
+call %PYTHON%\python.exe -m unittest test_variants
 
 popd
