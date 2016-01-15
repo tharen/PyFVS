c91603e6b2e68ed8c13407b7f1eed21ca60d8b0e
Tod Haren <tod.haren@gmail.com>
2016-01-14 16:12:59 -0800

    PYTHONPATH issues with appveyor test scripts.

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -1,8 +1,8 @@
 %PYTHON%\Scripts\pip install --upgrade nose-parameterized
 
-set PYTHONPATH="%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python;%PYTHONPATH%"
+set PYTHONPATH=%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python;%PYTHONPATH%
 
-pushd "%APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python\test"
+pushd %APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python\test
 call %PYTHON%\python.exe -m unittest test_variants
 
 popd
