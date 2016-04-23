6fa8feaebfca28dc104e5ef051646e2de0f3826e
Tod Haren <tod.haren@gmail.com>
2016-04-22 17:55:40 -0700

    Update configure_build.sh

--- config_build.sh
+++ config_build.sh
@@ -7,4 +7,4 @@ cd build
 cmake -G"Unix Makefiles" .. -DFVS_VARIANTS="pnc;wcc" -DWITH_PYEXT=ON \
     -D32BIT_TARGET=No -DNATIVE_ARCH=Yes -DSTATIC_LINK=ON \
     -DENABLE_LTO=OFF -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_INSTALL_PREFIX=$(pwd)/dist
+    -DCMAKE_INSTALL_PREFIX=$(pwd)/Open-FVS
