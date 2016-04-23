27bfc31252a79d0f6242bc5fc794436981e21e15
Tod Haren <tod.haren@gmail.com>
2016-04-22 17:46:34 -0700

    Update configure_build.sh

--- config_build.sh
+++ config_build.sh
@@ -1,10 +1,10 @@
 # Build PyFVS using Travis-CI
 
 cd bin
-mkdir -p travis-ci
-cd travis-ci
+mkdir -p build
+cd build
 
-cmake -G"Unix Makefiles" .. -DFVS_VARIANTS="pnc;wcc" -DWITH_PYMOD=ON \
+cmake -G"Unix Makefiles" .. -DFVS_VARIANTS="pnc;wcc" -DWITH_PYEXT=ON \
     -D32BIT_TARGET=No -DNATIVE_ARCH=Yes -DSTATIC_LINK=ON \
     -DENABLE_LTO=OFF -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_INSTALL_PREFIX=$(pwd)/Open-FVS
+    -DCMAKE_INSTALL_PREFIX=$(pwd)/dist
