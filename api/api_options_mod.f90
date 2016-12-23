1330711d1d9933555bff553e666673721065621b
Tod Haren <tod.haren@gmail.com>
2016-12-23 00:18:48 -0800

    Create option to switch between original FVS mortality routine and the newer ORGANON routine.

---
+++ api/api_options_mod.f90
@@ -0,0 +1,9 @@
+! Variables to control API options
+module api_options_mod
+    implicit none
+
+    logical use_fvs_morts
+
+    contains
+
+end module api_options_mod
--- wc/src/morts.f90
+++ wc/src/morts.f90
@@ -1,11 +1,14 @@
 subroutine morts
+! Replaces the base morts routine to switch between the original FVS
+! mortality routines and the ported ORGANON routines.
+
     use contrl_mod, only: use_fvs_morts
     implicit none
 
-    if use_fvs_morts==.true. then
+    if (use_fvs_morts) then
         call morts_fvs
     else
         call morts_organon
     end if
-
+    
 end subroutine morts
