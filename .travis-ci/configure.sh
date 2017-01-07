2f4eb198a04a49272f7fe7bae3fefa35d28eee81
Tod Haren <tod.haren@gmail.com>
2017-01-06 18:50:11 -0800

    Add SOC and CAC variants to Travis build.

--- .travis-ci/configure.sh
+++ .travis-ci/configure.sh
@@ -1,8 +1,9 @@
 mkdir -p ${BUILD_ROOT}
 pushd ${BUILD_ROOT}
 cmake -G"Unix Makefiles" .. \
-    -DFVS_VARIANTS="pnc;wcc" \
+    -DFVS_VARIANTS="pnc;wcc;soc;cac" \
     -DWITH_PYEXT=Yes \
     -DCMAKE_SYSTEM_NAME=Linux \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=Open-FVS    
 popd
