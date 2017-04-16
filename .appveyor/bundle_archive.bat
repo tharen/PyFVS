a70c6568307cb22a5b9fe1a423e101a192d20fa0
Tod Haren <tod.haren@gmail.com>
2017-04-16 01:19:20 -0700

    Enable OC variant in CI builds.

--- .appveyor/bundle_archive.bat
+++ .appveyor/bundle_archive.bat
@@ -1,4 +1,4 @@
-set ARCHIVE_NAME=open-fvs-%APPVEYOR_BUILD_VERSION%-Python%PYTHON_VERSION%_%PYTHON_ARCH%-windows-ci.zip
+set ARCHIVE_NAME=pyfvs-%APPVEYOR_BUILD_VERSION%-Python%PYTHON_VERSION%_%PYTHON_ARCH%-windows-ci.zip
 cd bin\\build
 7z a -bd %APPVEYOR_BUILD_FOLDER%\%ARCHIVE_NAME% Open-FVS\*
 popd
