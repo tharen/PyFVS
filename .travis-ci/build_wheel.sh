f15ae3420d63a5e63bf51cdf668b23dda35f4c53
Tod Haren <tod.haren@gmail.com>
2017-04-16 01:36:01 -0700

    Fix travis-ci wheel script.
    [skip appveyor]

--- .travis-ci/build_wheel.sh
+++ .travis-ci/build_wheel.sh
@@ -7,7 +7,7 @@ python setup.py bdist_wheel
 # Test the shiny new wheel
 cd dist
 pip install --no-index --find-links . pyfvs
-fvs --run-tests
+pyfvs --run-tests
 mv *.whl ${BUILD_ROOT}/artifacts
 
 popd
