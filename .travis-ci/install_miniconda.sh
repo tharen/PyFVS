bf36345827b652d1fcf2f4baab54e0cbf76287a9
Tod Haren <tod.haren@gmail.com>
2017-05-19 02:03:00 -0700

    Fixup CI builds
      - Add caching to Travis-CI
      - Add Matplotlib "agg" backend so examples scripts will run.

---
+++ .travis-ci/install_miniconda.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Use miniconda in travis-ci http://conda.pydata.org/docs/travis.html
+# We do this conditionally because it saves us some downloading if the
+# version is the same.
+if test -e $HOME/miniconda/bin ; then
+    echo "Miniconda is already installed."
+
+else
+    echo "Installing Miniconda."
+    
+    rm -rf $HOME/miniconda
+    mkdir -p $HOME/download
+    
+    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+      wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O $HOME/download/miniconda.sh;
+
+    else
+      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/download/miniconda.sh;
+    fi
+    
+    chmod +x $HOME/download/miniconda.sh
+    $HOME/download/miniconda.sh -b -p $HOME/miniconda
+
+fi
+
