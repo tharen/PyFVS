45680d9b404f45e2069b270992892f571d8e624d
Tod Haren <tod.haren@gmail.com>
2017-05-25 12:37:35 -0700

    Update PYPI distribution scripts.

--- .appveyor/deploy_pypi.bat
+++ .appveyor/deploy_pypi.bat
@@ -3,14 +3,14 @@
 
 pushd %APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python
 
-if %APPVEYOR_REPO_BRANCH%=="dev" (
+if "%APPVEYOR_REPO_BRANCH%" == "dev" (
     echo On dev branch, deploy to PYPI test.
     set TWINE_REPOSITORY_URL=https://testpypi.python.org/pypi
     goto upload
 )
 
-if %APPVEYOR_REPO_BRANCH%=="master" (
-    if %APPVEYOR_REPO_TAG%=="true" (
+if "%APPVEYOR_REPO_BRANCH%" == "master" (
+    if "%APPVEYOR_REPO_TAG%" == "true" (
         echo Tagged commit on master, deploying to pypi.
         set TWINE_REPOSITORY_URL=https://pypi.python.org/pypi
         goto upload
