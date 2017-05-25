db388a64fab2c8b09412e9fcf768380dcd65aef2
Tod Haren <tod.haren@gmail.com>
2017-05-24 18:00:50 -0700

    Update PYPI distribution scripts.

--- .travis-ci/deploy_pypi.sh
+++ .travis-ci/deploy_pypi.sh
@@ -7,8 +7,11 @@ pushd ${TRAVIS_BUILD_DIR}/artifacts
 
 if [ $TRAVIS_BRANCH = 'dev' ]; then
   TWINE_REPOSITORY_URL=https://testpypi.python.org
-else
+elif [ $TRAVIS_BRANCH = 'master' ] && [ -z ${TRAVIS_TAG+x}]; then
   TWINE_REPOSITORY_URL=https://pypi.python.org
+else
+  popd
+  exit
 fi
 
 twine upload *.whl --skip-existing
