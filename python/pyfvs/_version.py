6a4924f66bf17997559f5c8cbf181221c0401566
Tod Haren <tod.haren@gmail.com>
2017-06-20 11:02:25 -0700

    Fix "OPEN FAILED FOR   17" warning caused by long keyword file paths.
      - Increase the size of the CNAME variable from 40 to 250 characters.

--- python/pyfvs/_version.py
+++ python/pyfvs/_version.py
@@ -1,6 +1,6 @@
 # This file is automatically updated during packaging.
 # The version is extracted from the current Git tag using setup.py version.
 
-__version__ = '0.0.13dev5'
+__version__ = '0.0.13dev6'
 __git_describe__ = ''
 __git_commit__ = ''
