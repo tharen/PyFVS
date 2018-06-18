0dd583c5c6a34c49a3fc7e2014833ca6c2e53fa1
Tod Haren <tod.haren@gmail.com>
2018-06-18 15:38:31 -0700

    Update PYPY URLs.

--- .appveyor/deploy_pypi.bat
+++ .appveyor/deploy_pypi.bat
@@ -6,31 +6,27 @@ pushd %APPVEYOR_BUILD_FOLDER%\bin\build\Open-FVS\python
 
 if "%APPVEYOR_REPO_BRANCH%" == "dev" (
     echo On dev branch, deploy to PYPI test.
-    set TWINE_REPOSITORY=https://testpypi.python.org/pypi
-    set TWINE_REPOSITORY_URL=https://testpypi.python.org/pypi
-    goto upload
+    
+    echo Upload dev package to test.pypi.org.
+    call twine upload dist/* --repository-url https://test.pypi.org/legacy/ && (
+        echo twine upload complete.
+    ) || (
+        echo twine upload failed!
+    )
 )
 
 if "%APPVEYOR_REPO_BRANCH%" == "master" (
     if "%APPVEYOR_REPO_TAG%" == "true" (
-        echo Tagged commit on master, deploying to pypi.
-        set TWINE_REPOSITORY=https://pypi.python.org/pypi
-        set TWINE_REPOSITORY_URL=https://pypi.python.org/pypi
-        goto upload
-    )
+        echo Upload package to pypi.org.
+        call twine upload dist/* && (
+            echo twine upload complete.
+        ) || (
+            echo twine upload failed!
+        )
 )
 
-echo Not a tag on master, nor dev, skipping pypi deploy.
+echo Not on dev and not a tag on master, skipping pypi deploy.
 goto exit
-
-:upload
-echo Call twine to upload packages.
-call twine upload dist/* --skip-existing && (
-    echo twine upload complete.
-    (call )
-) || (
-    echo twine upload failed!
-)
 
 :exit
 popd
