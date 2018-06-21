653dc2f28fc310a360129ba6b76717bf3e5620df
Tod Haren <tod.haren@gmail.com>
2018-06-21 10:23:00 -0700

    [CI] Fix path variables.

--- .appveyor/setup_conda.bat
+++ .appveyor/setup_conda.bat
@@ -1,5 +1,3 @@
-set PATH=%MINICONDA%;%MINICONDA%\Scripts;%PATH%
-set PATH=%MINGW_PATH%\bin;%PATH%
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
 conda update -q conda
--- .travis-ci/setup_conda.sh
+++ .travis-ci/setup_conda.sh
@@ -1,4 +1,3 @@
-export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
 
