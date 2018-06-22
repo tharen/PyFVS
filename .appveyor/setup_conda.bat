ca2df3cb996266f6c6544f8f3d02018e13ae1344
Tod Haren <tod.haren@gmail.com>
2018-06-21 17:55:39 -0700

    [CI] Debug Python 3.4 config (appveyor).

--- .appveyor/setup_conda.bat
+++ .appveyor/setup_conda.bat
@@ -1,7 +1,7 @@
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
 conda --version
-conda update -q conda
+conda update -y -q conda
 conda --version
 
 REM Edit the environment.yml file for the target Python version
