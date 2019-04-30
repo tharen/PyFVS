11501f62a668bd2f389fc631071d6415d2a794c2
Tod Haren <tharen@users.noreply.github.com>
2019-04-30 10:23:01 -0700

    Remove deprecated conda create arguments.

--- .travis-ci/setup_conda.sh
+++ .travis-ci/setup_conda.sh
@@ -8,7 +8,7 @@ conda config --add channels conda-forge
 
 export pkg="numpy pandas cython pyodbc click setuptools wheel sphinx alabaster matplotlib pip twine"
 
-conda create -y -q --update-deps --name pyfvs python=$TRAVIS_PYTHON_VERSION $pkg
+conda create -y -q --name pyfvs python=$TRAVIS_PYTHON_VERSION $pkg
 source activate pyfvs
 pip install pytest sphinxcontrib-napoleon
 
