0800da1f70acff9edf9fc0874caf5b608bcf7185
Tod Haren <tod.haren@gmail.com>
2019-05-13 14:42:22 -0700

    Add docutils>=0.13.1 requirement.

--- .appveyor/setup_conda.bat
+++ .appveyor/setup_conda.bat
@@ -9,7 +9,7 @@ REM call sed -i -E 's/(python=)(.*)/\1'%PYTHON_VERSION%'/' ./environment.yml
 REM Create the conda environment
 REM conda env create -q -n pyfvs -f %APPVEYOR_BUILD_FOLDER%\\environment.yml
 
-set pkg=numpy pandas cython pyodbc click setuptools wheel sphinx alabaster matplotlib pip twine
+set pkg=numpy pandas cython pyodbc click setuptools wheel sphinx alabaster matplotlib pip twine docutils>=0.13.1
 conda create -y -q -n=pyfvs python=%PYTHON_VERSION% %pkg%
 call activate pyfvs
 pip install pytest sphinxcontrib-napoleon
--- .travis-ci/setup_conda.sh
+++ .travis-ci/setup_conda.sh
@@ -6,10 +6,10 @@ conda config --add channels conda-forge
 # sed -i -E 's/(python=)(.*)/\1'$TRAVIS_PYTHON_VERSION'/' ./environment.yml
 # conda env create --force -q -n pyfvs -f ${TRAVIS_BUILD_DIR}/environment.yml
 
-export pkg="numpy pandas cython pyodbc click setuptools wheel sphinx alabaster matplotlib pip twine"
+export pkg="numpy pandas cython pyodbc click setuptools wheel sphinx alabaster matplotlib pip twine docutils>=0.13.1"
 
 conda create -y -q --name pyfvs python=$TRAVIS_PYTHON_VERSION $pkg
 source activate pyfvs
-pip install docutils pytest sphinxcontrib-napoleon
+pip install pytest sphinxcontrib-napoleon
 
 conda info -a
